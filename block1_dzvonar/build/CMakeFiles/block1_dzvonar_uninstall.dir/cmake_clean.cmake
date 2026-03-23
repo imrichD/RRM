c6f138cec05ece8355a209349f707fcbdad45c7e
ImrichDzvonar <imrichdzvonar@gmail.com>
2026-03-23 10:16:19 +0100

    Stop tracking build folder and add to gitignore

--- block1_dzvonar/build/CMakeFiles/block1_dzvonar_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/block1_dzvonar_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/block1_dzvonar_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- block1_dzvonar/build/CMakeFiles/block1_dzvonar_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for block1_dzvonar_uninstall.
-# This may be replaced when dependencies are built.
